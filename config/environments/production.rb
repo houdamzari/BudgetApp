671b606dc1e3ef414f751faaeb75c8622c01c70e
houdamzari <houda.mzari1999@gmail.com>
2023-04-29 14:20:31 +0100

    Update production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || 
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
